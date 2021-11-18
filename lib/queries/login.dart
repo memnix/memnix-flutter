29d6556e34c997b1ef79e678f325f59af4764aa2
Yume <yume.network@protonmail.com>
2021-11-18 17:35:47 +0100

    lowercase email

--- lib/queries/login.dart
+++ lib/queries/login.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 Future<String?> attemptLogIn(String email, String password) async {
   var res = await http.post(Uri.parse('https://memnix.yumenetwork.net/api/login'),
       headers: {"Content-Type": "application/json"},
-      body: json.encode({'email': email, 'password': password}));
+      body: json.encode({'email': email.toLowerCase(), 'password': password}));
   if (res.statusCode == 200) {
     var cookie = res.headers['set-cookie'];
     return cookie;
