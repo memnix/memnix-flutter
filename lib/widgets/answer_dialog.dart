76ee36516080c8e8546033e4f006f5061ae50bba
Yume <yume.network@protonmail.com>
2021-11-20 11:49:10 +0100

    Update

--- lib/widgets/answer_dialog.dart
+++ lib/widgets/answer_dialog.dart
@@ -12,7 +12,7 @@ class AnswerDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     MaterialColor color;
-    if (jsonDecode(res!)["data"]["validate"] == true) {
+    if (jsonDecode(res!)["data"]["valdate"] == true) {
       color = Colors.green ;
     } else {
       color = Colors.red;
