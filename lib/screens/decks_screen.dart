5b4a41e4afcd44034bd9abc2ac3ae893c8a52c61
Yume <yume.network@protonmail.com>
2021-11-21 08:19:22 +0100

    Deck screen

--- lib/screens/decks_screen.dart
+++ lib/screens/decks_screen.dart
@@ -3,8 +3,8 @@ import 'package:google_fonts/google_fonts.dart';
 
 import '../bar.dart';
 
-class CardsPage extends StatelessWidget {
-  const CardsPage({Key? key}) : super(key: key);
+class DecksPage extends StatelessWidget {
+  const DecksPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
